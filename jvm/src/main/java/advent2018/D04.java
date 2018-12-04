feaeedab22cd1cd222488d03511e723025ffc11b
Konstantin Spirin <konstmsu@gmail.com>
2018-12-04 23:57:10 +0800

    Solved day 4 in Java

--- jvm/src/main/java/advent2018/D04.java
+++ jvm/src/main/java/advent2018/D04.java
@@ -1,10 +1,159 @@
 package advent2018;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class D04 {
-    public static int solve1(List<String> input) {
-        return 0;
+    public static class Solution {
+        public final int solution1;
+        public final int solution2;
+
+        public Solution(int solution1, int solution2) {
+            this.solution1 = solution1;
+            this.solution2 = solution2;
+        }
+    }
+
+    public static Solution solve(List<String> unorderedLines) {
+        var lines = unorderedLines.stream().sorted().collect(Collectors.toList());
+
+        class Context {
+            final List<String> lines;
+            int lineIndex;
+            final HashMap<Integer, List<Integer>> fallAsleep = new HashMap<>();
+            final HashMap<Integer, List<Integer>> wakeUp = new HashMap<>();
+            Integer currentGuardId;
+
+            Context(List<String> lines) {
+                this.lines = lines;
+            }
+
+            public Set<Integer> guardIds() {
+                return fallAsleep.keySet();
+            }
+
+            boolean hasReachedEnd() {
+                return lineIndex >= lines.size();
+            }
+
+            void moveNext() {
+                if (hasReachedEnd())
+                    throw new IllegalStateException();
+
+                lineIndex++;
+            }
+
+            boolean tryRecordAsNewGuard() {
+                var l = getCurrentLine();
+                var hash = l.indexOf('#');
+                if (hash < 0)
+                    return false;
+                currentGuardId = Integer.parseInt(l, hash + 1, l.indexOf(' ', hash + 2), 10);
+                moveNext();
+                return true;
+            }
+
+            void recordAsFallAsleep() {
+                String cl = getCurrentLine();
+                if (!cl.endsWith("falls asleep"))
+                    throw new IllegalStateException();
+
+                fallAsleep.computeIfAbsent(currentGuardId, ArrayList::new).add(extractTimestamp(cl));
+                moveNext();
+            }
+
+            void recordAsWakeUp() {
+                String cl = getCurrentLine();
+                if (!cl.endsWith("wakes up"))
+                    throw new IllegalStateException();
+
+                wakeUp.computeIfAbsent(currentGuardId, ArrayList::new).add(extractTimestamp(cl));
+                moveNext();
+            }
+
+            public void populate() {
+                while (!hasReachedEnd()) {
+                    if (!tryRecordAsNewGuard()) {
+                        recordAsFallAsleep();
+                        recordAsWakeUp();
+                    }
+                }
+            }
+
+            public String getCurrentLine() {
+                return lines.get(lineIndex);
+            }
+
+            class MinuteSleepTimes {
+                public final int minute;
+                public final int sleepTimes;
+
+                public MinuteSleepTimes(int minute, int sleepTimes) {
+                    this.minute = minute;
+                    this.sleepTimes = sleepTimes;
+                }
+            }
+
+            private int getSolution1() {
+                int maxTotalSleep = -1;
+                int maxId = -1;
+                for (var id: this.guardIds()) {
+                    int totalSleep = 0;
+                    for (var i = 0; i < this.fallAsleep.get(id).size(); i++)
+                        totalSleep += this.wakeUp.get(id).get(i) - this.fallAsleep.get(id).get(i);
+
+                    if(totalSleep > maxTotalSleep) {
+                        maxId = id;
+                        maxTotalSleep = totalSleep;
+                    }
+                }
+
+                MinuteSleepTimes longestMinute = getGuardTopSleepingMinute(maxId);
+
+                return longestMinute.minute * maxId;
+            }
+
+            private Context.MinuteSleepTimes getGuardTopSleepingMinute(int guardId) {
+                int[] sleeps = new int[60];
+                var from = this.fallAsleep.get(guardId);
+                var to = this.wakeUp.get(guardId);
+                for(var i = 0; i < from.size(); i++) {
+                    for (var m = from.get(i); m < to.get(i); m++)
+                        sleeps[m % 100]++;
+                }
+
+                int maxMinute = -1;
+                int maxSleep = -1;
+                for(var m = 0; m < sleeps.length; m++) {
+                    if (maxSleep < sleeps[m]) {
+                        maxMinute = m;
+                        maxSleep = sleeps[m];
+                    }
+                }
+
+                return new MinuteSleepTimes(maxMinute, maxSleep);
+            }
+
+            public int getSolution2() {
+                var topId = -1;
+                MinuteSleepTimes topTopMinute = null;
+
+                for(var id : guardIds()){
+                    var minute = getGuardTopSleepingMinute(id);
+                    if (topTopMinute == null || topTopMinute.sleepTimes<minute.sleepTimes) {
+                        topTopMinute = minute;
+                        topId = id;
+                    }
+                }
+
+                return topId * topTopMinute.minute;
+            }
+        }
+
+        var context = new Context(lines);
+        context.populate();
+
+        return new Solution(context.getSolution1(), context.getSolution2());
     }
 
     public static int extractTimestamp(String line) {
--- jvm/src/test/java/advent2018/test/D04Tests.java
+++ jvm/src/test/java/advent2018/test/D04Tests.java
@@ -1,8 +1,11 @@
 package advent2018.test;
 
 import advent2018.D04;
+import advent2018.Utils;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,8 +18,8 @@ public class D04Tests {
     }
 
     @Test
-    public void sample1() {
-        assertThat(D04.solve1(List.of(
+    public void sample() {
+        var result = D04.solve(List.of(
                 "[1518-11-01 00:00] Guard #10 begins shift",
                 "[1518-11-01 00:05] falls asleep",
                 "[1518-11-01 00:25] wakes up",
@@ -34,6 +37,15 @@ public class D04Tests {
                 "[1518-11-05 00:03] Guard #99 begins shift",
                 "[1518-11-05 00:45] falls asleep",
                 "[1518-11-05 00:55] wakes up"
-        )));
+        ));
+        assertThat(result.solution1).isEqualTo(240);
+        assertThat(result.solution2).isEqualTo(4455);
+    }
+
+    @Test
+    public void input() throws IOException {
+        D04.Solution result = D04.solve(Files.readAllLines(Utils.of("d04.txt")));
+        assertThat(result.solution1).isEqualTo(109659);
+        assertThat(result.solution2).isEqualTo(54);
     }
 }
