80e1bc663ad29083add48be47dda32804bec1daf
Konstantin Spirin <konstmsu@gmail.com>
2018-12-07 23:53:40 +0800

    Day 7 part 2 in Java

--- jvm/src/main/java/advent2018/D07.java
+++ jvm/src/main/java/advent2018/D07.java
@@ -1,9 +1,10 @@
 package advent2018;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeMap;
+import com.google.common.primitives.Ints;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class D07 {
     public static String solve1(List<String> rows) {
@@ -16,7 +17,6 @@ public class D07 {
         }
         var result = "";
         while (!prerequisites.isEmpty()) {
-            var found = false;
             for (var c : prerequisites.keySet()) {
                 if (prerequisites.get(c).isEmpty()) {
                     result += c;
@@ -24,14 +24,75 @@ public class D07 {
                     for (var s : prerequisites.values()) {
                         s.remove(c);
                     }
-                    found = true;
                     break;
                 }
             }
-            if (!found) {
-                throw new IllegalStateException();
-            }
         }
         return result;
+    }
+
+    public static int solve2(int workerCount, int durationOffset, List<String> rows) {
+        TreeMap<Character, HashSet<Character>> prerequisites = new TreeMap<>();
+        for (var r : rows) {
+            char first = r.charAt(5);
+            char then = r.charAt(36);
+            prerequisites.computeIfAbsent(first, v -> new HashSet<>());
+            prerequisites.computeIfAbsent(then, v -> new HashSet<>()).add(first);
+        }
+        var totalTime = 0;
+        var workers = new int[workerCount];
+        var assignments = new Character[workerCount];
+
+        while (!prerequisites.isEmpty()) {
+            Character nextJob = null;
+            for (var c : prerequisites.keySet()) {
+                if (prerequisites.get(c).isEmpty()) {
+                    nextJob = c;
+                    break;
+                }
+            }
+
+            if (nextJob != null) {
+                var foundWorker = false;
+                for (var j = 0; j < workers.length; j++) {
+                    if (workers[j] == 0) {
+                        System.out.printf("Assigned job %s to worker %d%n", nextJob, j);
+                        workers[j] = nextJob - 'A' + 1 + 60;
+                        assignments[j] = nextJob;
+                        prerequisites.remove(nextJob);
+                        foundWorker = true;
+                        break;
+                    }
+                }
+                if (foundWorker)
+                    continue;
+            }
+
+            System.out.println(Arrays.stream(workers).mapToObj(v -> v + "").collect(Collectors.joining(" ")));
+            var rem = Integer.MAX_VALUE;
+            for (var i = 0; i < workerCount; i++) {
+                if (assignments[i] != null)
+                    rem = Math.min(rem, workers[i]);
+            }
+
+            assert rem > 0;
+
+            System.out.printf("Working for %d...%n", rem);
+            totalTime += rem;
+            for (var j = 0; j < workers.length; j++) {
+                if (assignments[j] == null)
+                    continue;
+
+                workers[j] -= rem;
+                if (workers[j] == 0) {
+                    for (var s : prerequisites.values()) {
+                        s.remove(assignments[j]);
+                    }
+                    assignments[j] = null;
+                }
+            }
+        }
+
+        return totalTime + Ints.max(workers);
     }
 }
--- jvm/src/test/java/advent2018/test/D07Tests.java
+++ jvm/src/test/java/advent2018/test/D07Tests.java
@@ -25,6 +25,25 @@ public class D07Tests {
 
     @Test
     public void input1() throws IOException {
-        assertThat(D07.solve1(Files.readAllLines(Utils.of("d07.txt")))).isEqualTo("hello");
+        assertThat(D07.solve1(Files.readAllLines(Utils.of("d07.txt")))).isEqualTo("BFKEGNOVATIHXYZRMCJDLSUPWQ");
+    }
+
+    @Test
+    public void sample2() {
+        assertThat(D07.solve2(2, 0, List.of(
+                "Step C must be finished before step A can begin.",
+                "Step C must be finished before step F can begin.",
+                "Step A must be finished before step B can begin.",
+                "Step A must be finished before step D can begin.",
+                "Step B must be finished before step E can begin.",
+                "Step D must be finished before step E can begin.",
+                "Step F must be finished before step E can begin."))).isEqualTo(15);
+    }
+
+    @Test
+    public void input2() throws IOException {
+        int result = D07.solve2(5, 60, Files.readAllLines(Utils.of("d07.txt")));
+        assertThat(result).isLessThan(1800);
+        assertThat(result).isEqualTo(1020);
     }
 }
