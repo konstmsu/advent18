df2ac6bcd5b1e43a88f8886705cd270cae70b15f
Konstantin Spirin <konstmsu@gmail.com>
2018-12-07 22:47:18 +0800

    Day 6 in Java and some futile attempts in q

--- jvm/src/main/java/advent2018/D06.java
+++ jvm/src/main/java/advent2018/D06.java
@@ -1,16 +1,142 @@
 package advent2018;
 
-import java.util.Arrays;
+import com.google.common.primitives.Ints;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class D06 {
-    public static int solve1(int... pairs) {
-        int minX, maxX, minY, maxY;
-        minX = maxX = pairs[0];
-        minY = maxY = pairs[1];
 
-        var points = new int[pairs.length / 2];
-        for (var i = 0; i < pairs.length; i++) {
-            points[i] = pairs[i] * 1000 + pairs[i + 1];
+    public static int solve1(List<String> rows) {
+        var c = new Context(rows);
+        List<List<Integer>> wx = c.xs.stream().map(v -> Ints.asList(v)).collect(Collectors.toList());
+        List<List<Integer>> wy = c.ys.stream().map(v -> Ints.asList(v)).collect(Collectors.toList());
+
+        var generations = new int[c.dx][c.dy];
+        var sources = new int[c.dx][c.dy];
+        for (var s : sources)
+            Arrays.fill(s, -1);
+        var counter = new int[wx.size() + 1];
+
+        var equallyFar = wx.size();
+        assert equallyFar > 0;
+        for (var gen = 1; !wx.stream().flatMap(v -> v.stream()).findAny().isEmpty(); gen++) {
+            for (int source = 0; source < wx.size(); source++) {
+                assert source != equallyFar;
+                var xs = wx.get(source);
+                var ys = wy.get(source);
+                var nx = new ArrayList<Integer>();
+                var ny = new ArrayList<Integer>();
+                for (var i = 0; i < xs.size(); i++) {
+                    var x = xs.get(i);
+                    var y = ys.get(i);
+                    if (x < c.minx || y < c.miny || x > c.maxx || y > c.maxy)
+                        continue;
+
+                    if (sources[x - c.minx][y - c.miny] == -1) {
+                        generations[x - c.minx][y - c.miny] = gen;
+                        sources[x - c.minx][y - c.miny] = source;
+                        counter[source]++;
+
+                        nx.add(x);
+                        nx.add(x);
+                        nx.add(x + 1);
+                        nx.add(x - 1);
+
+                        ny.add(y + 1);
+                        ny.add(y - 1);
+                        ny.add(y);
+                        ny.add(y);
+                    } else if (generations[x - c.minx][y - c.miny] == gen && sources[x - c.minx][y - c.miny] != source) {
+                        counter[sources[x - c.minx][y - c.miny]]--;
+                        sources[x - c.minx][y - c.miny] = equallyFar;
+                    }
+                }
+                wx.set(source, nx);
+                wy.set(source, ny);
+            }
+        }
+
+        return Ints.max(counter);
+    }
+
+    public static int solve2(int maxTotalDistance, List<String> rows) {
+        var c = new Context(rows);
+        var ax = (int) Math.round(c.xs.stream().mapToInt(Integer::intValue).average().getAsDouble());
+        var ay = (int) Math.round(c.ys.stream().mapToInt(Integer::intValue).average().getAsDouble());
+        Queue<Integer> wx = new ArrayDeque<>(Ints.asList(ax));
+        Queue<Integer> wy = new ArrayDeque<>(Ints.asList(ay));
+        HashSet<Integer> seen = new HashSet<>();
+        int counter = 0;
+        while (!wx.isEmpty()) {
+            var x = wx.remove();
+            var y = wy.remove();
+
+            var k = x * maxTotalDistance + y;
+
+            if (seen.contains(k))
+                continue;
+
+            seen.add(k);
+
+            var d = 0;
+            for (var i = 0; i < c.xs.size(); i++) {
+                d += Math.abs(x - c.xs.get(i)) + Math.abs(y - c.ys.get(i));
+            }
+
+            if (d >= maxTotalDistance)
+                continue;
+
+            counter++;
+            wx.add(x);
+            wx.add(x);
+            wx.add(x + 1);
+            wx.add(x - 1);
+
+            wy.add(y + 1);
+            wy.add(y - 1);
+            wy.add(y);
+            wy.add(y);
+        }
+
+        return counter;
+    }
+
+    private static class Context {
+        private List<Integer> xs;
+        private List<Integer> ys;
+        public final int minx;
+        public final int miny;
+        public final int maxx;
+        public final int maxy;
+        public final int dx;
+        public final int dy;
+
+        public Context(List<String> rows) {
+            xs = new ArrayList<>();
+            ys = new ArrayList<>();
+            var tminx = Integer.MAX_VALUE;
+            var tminy = Integer.MAX_VALUE;
+            var tmaxx = Integer.MIN_VALUE;
+            var tmaxy = Integer.MIN_VALUE;
+            for (var r : rows) {
+                var parts = r.split(", ");
+                int x = Integer.parseInt(parts[0]);
+                int y = Integer.parseInt(parts[1]);
+                xs.add(x);
+                ys.add(y);
+                tminx = Math.min(tminx, x);
+                tminy = Math.min(tminy, y);
+                tmaxx = Math.max(tmaxx, x);
+                tmaxy = Math.max(tmaxy, y);
+            }
+            minx = tminx;
+            maxx = tmaxx;
+            miny = tminy;
+            maxy = tmaxy;
+            dx = tmaxx - tminx + 1;
+            dy = tmaxy - tminy + 1;
         }
     }
 }
--- jvm/src/test/java/advent2018/test/D06Tests.java
+++ jvm/src/test/java/advent2018/test/D06Tests.java
@@ -1,19 +1,47 @@
 package advent2018.test;
 
 import advent2018.D06;
+import advent2018.Utils;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class D06Tests {
     @Test
     public void sample1() {
-        assertThat(D06.solve1(
-                1, 1,
-                1, 6,
-                8, 3,
-                3, 4,
-                5, 5,
-                8, 9)).isEqualTo(17);
+        assertThat(D06.solve1(List.of(
+                "1, 1",
+                "1, 6",
+                "8, 3",
+                "3, 4",
+                "5, 5",
+                "8, 9"
+        ))).isEqualTo(17);
+    }
+
+    @Test
+    public void input1() throws IOException {
+        assertThat(D06.solve1(Files.readAllLines(Utils.of("d06.txt")))).isEqualTo(5365);
+    }
+
+    @Test
+    public void sample2() {
+        assertThat(D06.solve2(32, List.of(
+                "1, 1",
+                "1, 6",
+                "8, 3",
+                "3, 4",
+                "5, 5",
+                "8, 9"
+        ))).isEqualTo(16);
+    }
+
+    @Test
+    public void input2() throws IOException {
+        assertThat(D06.solve2(10000, Files.readAllLines(Utils.of("d06.txt")))).isEqualTo(42513);
     }
 }
